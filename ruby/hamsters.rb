0b2416fb7eb90bb3b5b29bd75d804686a3d7c1c6
verdesalmon <iferiav@gmail.com>
2017-05-09 14:24:34 -0500

    Add data type convertion and printing data

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,27 +1,19 @@
 puts "What's the hamster's name?"
-hamstername = gets.chomp
-puts "The Hamster's name is #{hamstername}!"
+hamstername = gets.chomp.capitalize
+
 puts "What's the volume of hamster between 1 & 10?"
 hamstervol = gets.chomp.to_i
-puts "The Hamster's volume is #{hamstervol}."
+ 
 puts "What's the hamster's fur color?"
 hamstercolor = gets.chomp
-puts "The color of the hamster's fur is a beautiful #{hamstercolor}."
+
 puts "Is the hamster worth adopting? (y or n)"
-hamsteradopt = gets.chomp
-if hamsteradopt == "y"
-  hamsteradopt = true
-  puts "The Hamster is worth adopting."
-else hamsteradopt == "n"
-  hamsteradopt = false
-  puts "Ew, the Hamster is not worth adopting."
-end
+hamsteradopt = gets.chomp #It's a new buil in .to_b that works in 2.4 ruby version
+hamsteradopt == "y"?  hamsteradopt ="worth" : hamsteradopt ="not worth"
+
 puts "What's the hamster's estimated age?"
-hamsterestage = gets.chomp
-if hamsterestage == ""
-  hamsterestage = nil
-  puts "The hamster's age is unknown."
-else
-  hamsterestage = hamsterestage.to_i
-  puts hamsterestage
-end
+hamsterestage = gets.chomp.to_i
+hamsterestage == nil ? nil : hamsterestage
+
+
+puts "The Hamster's name is #{hamstername}!,\nthe volume is #{hamstervol},\nthe fur color is a beautiful #{hamstercolor},\nthe estimated age is #{hamsterestage}\nand the hamster is #{hamsteradopt} adopting."
