dc24f2b8557bd72daf40e6a9e7054dca80805318
verdesalmon <iferiav@gmail.com>
2017-05-09 17:07:45 -0500

    Add questions for user input

---
+++ ruby/vampires.rb
@@ -0,0 +1,17 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "What year were you born?" 
+b_year = gets.chomp
+
+puts "What is your age?"
+age = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance?"
+health = gets.chomp
+
+
+
