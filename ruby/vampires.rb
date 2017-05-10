87f9953dc7a07d436c3552d4c2e1465d7b0d526b
verdesalmon <iferiav@gmail.com>
2017-05-09 20:44:12 -0500

    Try ternary conditions

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,41 +1,49 @@
-
 puts "What is your name?"
 name = gets.chomp
+name == "Drake Cula" && name == "Tu Fang"? name = true : name = false
+name2 = name
 
 #puts "What year were you born?" 
 #b_year = gets.chomp.to_i
 
 puts "What is your age?"
 age = gets.chomp.to_i
+age < 100? age =true : age =false
+age2 = age
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)" 
 garlic = gets.chomp
-garlic == "y"?   garlic =true : garlic =false
+garlic == "y"? garlic =true : garlic =false
 
 puts "Would you like to enroll in the company’s health insurance?(y/n)"
 h_insur = gets.chomp
 h_insur == "y"? h_insur =true : h_insur =false
 
 
-test2 =[name, age, garlic, h_insur]
-#puts "Probably not a vampire." if age <= 100 && (garlic || h_insur)
-#puts "Probably a vampire."
+#test2 =[name, age, garlic, h_insur]
 
-#age < 100 && (garlic||h_insur)? (puts "Probably not a vampire."):(puts "Probably a vampire.")
-#age >100 && garlic && h_insur ? "Almost certainly a vampire."
-#puts "Definitely a vampire." if name == "Drake Cula" || "Tu Fang"
-#if age >100 && garlic && h_insur && name != "Drake Cula" || "Tu Fang"; puts "Results inconclusive."
 
-case test2
-when age < 100 && (garlic||h_insur)
-	puts "Probably not a vampire."
-when age >100 && (garlic||h_insur)
-	puts "Probably a vampire."
-when age >100 && garlic && h_insur
-	puts "Almost certainly a vampire."
-when name == "Drake Cula" || "Tu Fang"
-	puts "Almost certainly a vampire."
-end
+
+
+age2 && (garlic||h_insur)? (puts "Probably not a vampire."):(puts "Probably a vampire.")
+
+!age2 && !garlic && !h_insur ? "Almost certainly a vampire." : false
+
+puts "Definitely a vampire." if name2
+
+puts "Results inconclusive." if  age2 && garlic && h_insur && !name2 
+
+
+#case test2
+#when age < 100 && (garlic||h_insur)
+	#puts "Probably not a vampire."
+#when age >100 && (garlic||h_insur)
+	#puts "Probably a vampire."
+#when age >100 && garlic && h_insur
+	#puts "Almost certainly a vampire."
+#when name == "Drake Cula" || "Tu Fang"
+	#puts "Almost certainly a vampire."
+#end
 
 
 =begin
@@ -45,4 +53,3 @@ If the employee got their age wrong, hates garlic bread, and doesn’t want insu
 Even if the employee is an amazing liar otherwise, anyone going by the name of “Drake Cula” or “Tu Fang” is clearly a vampire, because come on. In that case, you should print “Definitely a vampire.”
 Otherwise, print “Results inconclusive.”
 =end
-
