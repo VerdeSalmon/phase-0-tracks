67c93cb91d5d3092224a8c6302788574c0f220dd
verdesalmon <iferiav@gmail.com>
2017-05-12 17:20:49 -0500

    Add a new condition to vampires.rb

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -10,9 +10,12 @@ until i == num_empl
 	name == "Drake Cula" || name == "Tu Fang"? name = true : name = false
 	
 	puts "What is your age? What year were you born?"
-	age = gets.chomp.to_i
-	0 < age && age < 100? age =true : age =false
-	
+	age = gets.chomp.split(' ').map(&:to_i)
+	0 < age[0] && age[0] < 100? age[0] = true : age[0] =false
+	(Time.new.year - 100) < age[1] && age[1] < Time.new.year ? age[1] =true : age[1] =false
+	age = age[0] && age[1]
+	puts age
+
 	puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)" 
 	garlic = gets.chomp
 	garlic == "y"? garlic =true : garlic =false
@@ -30,12 +33,12 @@ until i == num_empl
 	
 
 	
-	if !name && age && (garlic||h_insur)&& allergy != "sunshine" ; puts "Probably not a vampire."; #age true, garlic good or insurance good
+	if !name && age && (garlic||h_insur)&& allergy != "sunshine" ; puts "Probably not a vampire.";
 		
-	elsif !age && garlic || !age && h_insur; puts "Probably a vampire.";#age false , garlic no or insurance no
-		
-	elsif !age && !garlic && !h_insur; puts "Almost certainly a vampire.";#age false, garlic no and insurance no
-		
+	elsif !age && garlic || !age && h_insur; puts "Probably a vampire.";
+
+	elsif !age && !garlic && !h_insur; puts "Almost certainly a vampire.";
+
 	elsif name; puts "Definitely a vampire.";
 
 	elsif allergy == "sunshine"; puts "Probably a vampire.";
@@ -47,19 +50,6 @@ i += 1
 end
 
 puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
-
-
-
-
-
-
-=begin
-If the employee got their age right, and is willing to eat garlic bread or sign up for insurance, the result is “Probably not a vampire.”
-If the employee got their age wrong, and hates garlic bread or waives insurance, the result is “Probably a vampire.”
-If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, the result is “Almost certainly a vampire.”
-Even if the employee is an amazing liar otherwise, anyone going by the name of “Drake Cula” or “Tu Fang” is clearly a vampire, because come on. In that case, you should print “Definitely a vampire.”
-Otherwise, print “Results inconclusive.”
-=end
 
 
 
