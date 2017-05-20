45ff343f1f58fdc76a99615bda5081b2f19873e6
verdesalmon <iferiav@gmail.com>
2017-05-20 08:01:08 -0500

    Add name_swap and alias_maker methods

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,34 @@
+#Swapping the first and last name.
+#Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou'
+
+def name_swap(name)
+
+name_split = name.split(" ")
+name_swap = name_split.shuffle
+name_join = name_swap.join(" ")
+
+end
+
+
+def alias_maker(name_swaped)
+	
+	vowels = ["a","e","i","o","u"]
+	consonants = ("a".."z").to_a - vowels
+	name_lowercase = name_swaped.downcase
+	letter_split = name_lowercase.chars
+	
+
+	name_new = letter_split.map do |letter| 
+		if vowels.include? letter
+		vowels.rotate(1)[vowels.index(letter)]
+		elsif consonants.include? letter
+		consonants.rotate(1)[consonants.index(letter)]	
+		else 
+			letter
+		end
+	end 
+	name_new.join.split(" ").map(&:capitalize).join(" ")
+end
+
+
+p alias_maker(name_swap("Felicia Torres")) #Vussit Gimodoe
