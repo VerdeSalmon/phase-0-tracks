5f1a29a7915cda65cec7bef7cccf16647fa1261d
verdesalmon <iferiav@gmail.com>
2017-05-21 22:14:08 -0500

    Add solution Hash Drills

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -19,10 +19,9 @@ zombie_apocalypse_supplies.each_index do |supply_one|
 		zombie_apocalypse_supplies[supply_one], zombie_apocalypse_supplies[supply_two] = zombie_apocalypse_supplies[supply_two], zombie_apocalypse_supplies[supply_one]
 		end
 	end
-	if zombie_apocalypse_supplies.last == "water jug"
-		p zombie_apocalypse_supplies
-	end
 end
+p zombie_apocalypse_supplies
+
 
 # ----
 
@@ -37,9 +36,9 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 
 	zombie_apocalypse_supplies.each do |supply|
  		if supply == supplies
- 			p true
+ 			puts "#{supply}: true"
  		else
- 			p false
+ 			false
  		end
  	end
 end
@@ -90,16 +89,39 @@ extinct_animals = {
 
 # 1. Iterate through extinct_animals hash, printing each key/value pair
 # with a dash in between the key and value, and an asterisk between each pair.
+
+extinct_animals.each do |animal, extinction_year|
+		print "#{animal}-#{extinction_year} * "
+	end
+
 # ----
 
 # 2. Keep only animals in extinct_animals if they were extinct before
 # the year 2000. Do not use any special built-in methods.
+
+extinct_animals.each do |animal, extinction_year|
+ 		if extinction_year < 2000
+ 			puts animal
+ 		else
+ 			extinct_animals.delete(animal)
+ 		end
+ 	end
+
+ 	p extinct_animals
+
 # ----
 
 # 3. Our calculations were completely off, turns out all of those animals went
 # extinct 3 years before the date provided. Update the values in extinct_animals
 # so they accurately reflect what year the animal went extinct.
 # Do not use any special built-in methods.
+
+extinct_animals.each do |animal, extinction_year|
+ 		extinct_animals[animal] = extinction_year - 3
+ 	end
+
+ 	p extinct_animals
+
 # ----
 
 # 4. You've heard that the following animals might be extinct, but you're not sure.
@@ -108,10 +130,32 @@ extinct_animals = {
 # "Dodo"
 # "Saiga Antelope"
 # Do not use any special built-in methods.
+
+extinct_animals_finder = ["Andean Cat","Dodo","Saiga Antelope"]
+
+	extinct_animals.each_key do |extinct_animals|
+		extinct_animals_finder.each do |animal|
+ 			if extinct_animals == animal
+ 				puts "#{animal} : true"
+ 			else
+ 				false
+ 			end
+ 		end
+	end
+
 # ----
 
 # 5. We just found out that the Passenger Pigeon is actually not extinct!
 # Remove them from extinct_animals and return the key value pair as a two item array.
 # Find the built-in method that helps you accomplish this in the Ruby documentation
 # for Hashes.
+
+sort_extinct_animals = extinct_animals.sort_by {|animal, extinction_year| extinction_year}
+ 
+rotate_sort_extinct_animals = sort_extinct_animals.rotate(2)
+
+non_extinct_animals = rotate_sort_extinct_animals.shift
+
+
+
 # ----
