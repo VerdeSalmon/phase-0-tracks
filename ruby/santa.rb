a98c61f4726587c132fe62330dfec7f6bc8b5754
verdesalmon <iferiav@gmail.com>
2017-05-23 16:07:20 -0500

    Add class, instance methods and drive code

---
+++ ruby/santa.rb
@@ -0,0 +1,24 @@
+class Santa
+
+	def initialize
+		puts "Initialize Santa instance..."
+	end
+
+	def speak
+		puts "Ho,ho,ho! Haaaapy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie_type)
+		puts "That was a good #{cookie_type}"
+	end
+end
+
+
+
+#DRIVER CODE
+
+happy_santa = Santa.new
+
+happy_santa.speak
+
+happy_santa.eat_milk_and_cookies("chococaramel")
