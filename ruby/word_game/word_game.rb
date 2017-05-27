2820e87f8c11ff66575d404636cb32732037ca36
verdesalmon <iferiav@gmail.com>
2017-05-27 16:41:19 -0500

    Add class and empty initialize, empty instance methods

---
+++ ruby/word_game/word_game.rb
@@ -0,0 +1,35 @@
+
+class WordGame
+ 
+
+	def initialize
+	end
+
+
+	def string_to_guess(secret_string)
+
+	end
+
+	def check_word(guess_string)
+		
+	end
+
+
+	def print_end_feedback(tries)
+		
+	end
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
