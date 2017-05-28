9b3a51079d82ba7575e6c7ed5e9c7cbec00b211a
verdesalmon <iferiav@gmail.com>
2017-05-27 20:40:03 -0500

    Add three tests

--- ruby/word_game/word_game_spec.rb
+++ ruby/word_game/word_game_spec.rb
@@ -6,11 +6,34 @@ describe WordGame do
   let(:players) {WordGame.new}
 
   	it "initialize instance variables" do
-    expect().to eq [""]
+    expect(@letters_to_guess) == [@letters_to_guess]
  	end
-
 
   	it "split by characters and stores the new array in a instance variable" do
   	players.string_to_guess("My uncle steve")
-    expect(players.string_to_guess).to eq ["m", "y", " ", "u", "n", "c", "l", "e", " ", "S", "t", "e", "v", "e"]
+    expect(@letters_to_guess) == [["m", "y", " ", "u", "n", "c", "l", "e", " ", "S", "t", "e", "v", "e"]]
   	end
+
+  	it "split by characters, stores the new array in a variable, compare to arrays and store in a new array the equal elements in the same index" do
+  	players.string_to_guess("My uncle steve")
+  	players.check_word("e")	
+  	expect(@guess) == [["_ ", "_ ", "  ", "_ ", "_ ", "_ ", "_ ", "e", "  ", "_ ", "_ ", "e", "_ ", "e"]]
+  	end
+
+  	it "print message for the user when the game is over" do
+  	players.string_to_guess("My uncle steve")
+  	players.check_word("My uncle steve")
+  	expect(@letters_to_guess.eql? @guess).to be true
+  	end
+
+  	it "print message for the user when the game is over" do
+  	players.string_to_guess("My uncle steve")
+  	players.check_word("p")
+  	expect(@letters_to_guess.eql? @guess).to be false
+  	end
+
+ 
+end
+
+
+
