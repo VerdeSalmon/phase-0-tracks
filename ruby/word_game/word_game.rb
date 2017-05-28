25048495d0e7d86ee709b0748d98a1caafe7bb18
verdesalmon <iferiav@gmail.com>
2017-05-27 20:38:12 -0500

    Add two instance methods

--- ruby/word_game/word_game.rb
+++ ruby/word_game/word_game.rb
@@ -1,24 +1,45 @@
 
 class WordGame
  
+	attr_reader :letters_to_guess
+	attr_accessor :guess
+
 
 	def initialize
+		@letters_to_guess
+		@guess
 	end
 
 
 	def string_to_guess(secret_string)
-
+		@letters_to_guess = secret_string.downcase.chars
 	end
 
 	def check_word(guess_string)
-		
+		guess_letters = guess_string.downcase.chars
+		@guess = []
+		@letters_to_guess.each do |letter|
+		if guess_letters.include? letter
+			@guess << letter 
+		elsif letter == " "
+			@guess << "  "			
+		else 
+			@guess << "_ "
+		end
+	end
+	@guess
 	end
 
 
-	def print_end_feedback(tries)
-		
-	end
+	def print_end_feedback
 
+			if @letters_to_guess.eql? @guess 
+				puts "Amazing Job,\n YOU WIN!!!"
+			else 
+				puts "You lose the game, but you win a dictionary :D.\n Try Again!"
+			end
+	
+	end
 
 end
 
