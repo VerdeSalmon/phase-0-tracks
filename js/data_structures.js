ba6bc9c4806d5914a826293134f1309a1802f0ab
verdesalmon <iferiav@gmail.com>
2017-06-02 15:22:15 -0500

    Add a hash, using two arrays as key-value pair

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,10 +1,5 @@
 // Release 0: Work with Arrays
 
-// declare two separate arrays:
-// an array of four colors ("blue", for instance, though you're welcome to get crazy with something like "iridescent blue/green with silver sparkles")
-// an array of four names that you might give a horse ("Ed" is always a solid choice to start with)
-// write code that adds another color to the colors array, and another horse name to the horses array.
-
 
 var horseName = ["Ed", "Ginger","Glitter","Vanila"];
 var horseColor = ["blue", "yellow","pink blue stripes","rainbow"];
@@ -18,6 +13,16 @@ horseColor.push("green spots");
 // The keys of your object should be horse names, and the values should be colors. 
 // would work for any number of colors/horses, as long as the two arrays are the same length
 
+
+function horseMaker(horseName, horseColor){
+	var horsesMatcher = {};
+	for (var i = 0; i < horseName.length; i++){
+		horsesMatcher[horseName[i]] = horseColor[i];
+	}
+	return horsesMatcher
+}
+
+console.log(horseMaker(horseName,horseColor))
 
 // Release 2: Build Many Objects Using a Constructor
 
