b023138ae40d949e080fd366e75215d889d393e0
verdesalmon <iferiav@gmail.com>
2017-06-02 17:51:37 -0500

    Correcting 'undefined' output using built-in array methods

--- js/explore.js
+++ js/explore.js
@@ -1,26 +1,26 @@
-//Define a function that take a string as a parameter and returned reversed. 
+// Create an object that takes a word or phrase an reverse it
+
 function reverse(word){
-	//Iterate trough the word and coutndown by index
-	var reverseString = "";
-	for (i = word.length; i >= 0; i--) {
-	//Return each letter of the word
-	reverseString = (reverseString + word[i]);
+	// Create an array to return the reverse string
+	var reverseString = [];
+	// Iterate trhough the word
+	for (var i = 0; i < word.length; i++) {
+	// Return each letter of the word, put on top last letter returned
+	reverseString.unshift(word[i]);
 	} 
-	return reverseString;
+	//Return the reverse string
+	return reverseString.join("");
 }
 
 
-//Call the function, give a string as an argument and store the result of the function in a variable
-var reverseWord = reverse("blue");
+//Call the object, give a string as an argument and store the result of the function in a variable
+var reverseWord = reverse("yellow chicken baby");
 
 
 //Print the variable when a condition become true
 if (reverseWord.length > 5){
 	console.log(reverseWord);
 }
-
-
-
 
 
 
