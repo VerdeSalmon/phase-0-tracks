d37f773243297ef2ab85a7b9416d1964700cee6e
verdesalmon <iferiav@gmail.com>
2017-06-02 15:41:28 -0500

    Create a constructor function

--- js/data_structures.js
+++ js/data_structures.js
@@ -9,11 +9,6 @@ horseColor.push("green spots");
 
 // Release 1: Build an Object
 
-// add code that will use your two arrays to create an object.
-// The keys of your object should be horse names, and the values should be colors. 
-// would work for any number of colors/horses, as long as the two arrays are the same length
-
-
 function horseMaker(horseName, horseColor){
 	var horsesMatcher = {};
 	for (var i = 0; i < horseName.length; i++){
@@ -24,8 +19,40 @@ function horseMaker(horseName, horseColor){
 
 console.log(horseMaker(horseName,horseColor))
 
+console.log("----")
+
+
 // Release 2: Build Many Objects Using a Constructor
 
 // write a constructor function for a car. 
 // Give it a few different properties of various data types, including at least one function. 
 // Demonstrate that your function works by creating a few cars with it.
+
+
+function Car(modelYear, color, confortable){
+  this.modelYear = modelYear;
+  this.color = color;
+  this.confortable = confortable;
+  this.honk = function(){console.log("Beep Beep");};
+  }
+
+
+var car1 = new Car(2010, "pink blue stripes", true);
+console.log(car1);
+car1.honk();
+
+
+console.log("----")
+
+
+var car2 = new Car(1999, "tooth white", false);
+car2.honk();
+console.log(car2);
+
+
+console.log("----")
+
+
+var car3 = new Car(2020, "silver hope", false);
+car3.honk();
+console.log(car3);
