86d47c54492537f2c61f5139cf574e7f71f26756
verdesalmon <iferiav@gmail.com>
2017-06-02 15:09:21 -0500

    Add two arrays and add items to them

---
+++ js/data_structures.js
@@ -0,0 +1,26 @@
+// Release 0: Work with Arrays
+
+// declare two separate arrays:
+// an array of four colors ("blue", for instance, though you're welcome to get crazy with something like "iridescent blue/green with silver sparkles")
+// an array of four names that you might give a horse ("Ed" is always a solid choice to start with)
+// write code that adds another color to the colors array, and another horse name to the horses array.
+
+
+var horseName = ["Ed", "Ginger","Glitter","Vanila"];
+var horseColor = ["blue", "yellow","pink blue stripes","rainbow"];
+
+horseName.push("Joe");
+horseColor.push("green spots");
+
+// Release 1: Build an Object
+
+// add code that will use your two arrays to create an object.
+// The keys of your object should be horse names, and the values should be colors. 
+// would work for any number of colors/horses, as long as the two arrays are the same length
+
+
+// Release 2: Build Many Objects Using a Constructor
+
+// write a constructor function for a car. 
+// Give it a few different properties of various data types, including at least one function. 
+// Demonstrate that your function works by creating a few cars with it.
