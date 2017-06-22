9360bd73db873f0d2ab755073c09ae98be6fda1a
verdesalmon <iferiav@gmail.com>
2017-06-21 22:21:05 -0500

    Refactoring declare methods and define drive code and business code

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,59 +1,96 @@
-puts "How many employees you will be processed?"
-employees_to_process = gets.chomp.to_i 
+# VAMPIRE DETECTION PROGRAM
 
 
-employees_to_process.times do 
+# ------------ FUNTIONAL CODE -------------
 
-
-	puts "What is your name?"
-	name_input = gets.chomp
+def check_name(name_input)
 	name = name_input == "Drake Cula" || name_input == "Tu Fang"
+end
 
-	
-	puts "What is your age? What year were you born? (type age and year. example '34,1982'"
-	age = gets.chomp.split(',').map(&:to_i)
 
-		if 0 < age[0] && age[0] < 100 && (Time.new.year - 100) < age[1]
+def check_age(age_input)
+
+		age = age_input.split(',').map(&:to_i)
+
+		if 0 < age[0] && age[0] < 100 && (Time.new.year - 100) < age[1] && (Time.new.year - age[1]) == age[0]
 			age = true
 		else
 			age = false
 		end
-	
+end
 
-	puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)" 
-	garlic = gets.chomp
-	garlic == "y"? garlic =true : garlic =false
-	
-	puts "Would you like to enroll in the company’s health insurance?(y/n)"
-	health_insurance = gets.chomp
-	health_insurance == "y"? health_insurance =true : health_insurance =false
 
+def check_garlic(eat_garlic)
+	eat_garlic == "y"? eat_garlic =true : eat_garlic =false
+end
+
+
+def check_allergy
 	allergy = ""
 	while allergy != "sunshine" && allergy != "done"
-		puts "Do you have any allergy? Please type one at the time and type 'done' when you finish."
+		puts "Do you have any allergy? Please enter one allergy at the time and type 'done' when you finish."
 		allergy = gets.chomp.downcase
 	end
-	
+end
 
+
+def check_health_insurance(health_insurance)
+	health_insurance == "y"? health_insurance =true : health_insurance =false
+end
+
+
+def interview
+	puts "\nNew Employee"
+	puts "What is your name?"
+	name = check_name(gets.chomp)	
 	
-	if !name && age && (garlic||health_insurance)&& allergy != "sunshine" 
-		puts "Probably not a vampire."	
-	elsif !age && garlic || !age && health_insurance
-		puts "Probably a vampire."
-	elsif !age && !garlic && !health_insurance
-		puts "Almost certainly a vampire."
+	puts "What is your age? What year were you born? (type age and year. example '34,1982'"
+	age = check_age(gets.chomp)
+	
+	puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)" 
+	eat_garlic = check_garlic(gets.chomp)	
+
+	puts "Would you like to enroll in the company’s health insurance?(y/n)"
+	health_insurance = check_health_insurance(gets.chomp)
+
+	check_allergy
+
+
+	if !name && age && (eat_garlic||health_insurance)&& check_allergy != "sunshine" 
+		results = "Probably not a vampire."	
+	elsif !age && eat_garlic || !age && health_insurance
+		results = "Probably a vampire."
+	elsif !age && !eat_garlic && !health_insurance
+		results = "Almost certainly a vampire."
 	elsif name
-		puts "Definitely a vampire."
-	elsif allergy == "sunshine"
-		puts "Probably a vampire."
+		results = "Definitely a vampire."
+	elsif check_allergy == "sunshine"
+		results = "Probably a vampire."
 	else
-		"Results inconclusive."
+		results = "Results inconclusive."
 	end 
+
+	print_results(results)
 
 end
 
 
-puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
+def print_results(results)
+	puts "\nEmployee result: #{results}"
+end
+
+
+
+# ----------------- DRIVE CODE ------------------------
+
+
+puts "How many employees you will be processed?"
+employees_to_process = gets.chomp.to_i
+	
+employees_to_process.times{interview}
+
+
+puts "\nActually, never mind! What do these questions have to do with anything? Let's all be friends."
 
 
 
