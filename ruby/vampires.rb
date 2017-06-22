06697238b0f261adbc7172bc957704f05fe4e5ad
VerdeSalmon <iferiav@gmail.com>
2017-06-21 22:26:14 -0500

    Update vampires.rb

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,7 +1,7 @@
 # VAMPIRE DETECTION PROGRAM
 
 
-# ------------ FUNTIONAL CODE -------------
+# ------------ FUNCTIONAL CODE -------------
 
 def check_name(name_input)
 	name = name_input == "Drake Cula" || name_input == "Tu Fang"
