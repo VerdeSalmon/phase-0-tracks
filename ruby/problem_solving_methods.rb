e603fee67e3110bd4e4a69ca8057f2c6b836614f
verdesalmon <iferiav@gmail.com>
2017-07-04 13:58:46 -0500

    Refactor search_number method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,30 +1,32 @@
-#Release 0
+# Release 0
+
+
+# Create a method to search a number in a data structure and return it's index. 
+  # Input: It takes an array of integers and an integer as parameters.
+  # Iterate through the data structure
+    # Do conditional statements to check if the gived number is in the data structure
+      # If it's return the index of the number
+      # Else return nil 
+
+
+def search_number(number_list, number)
+  number_list.length.times do |index|
+    if number_list[index] == number
+      return index
+    end
+  end
+  return nil
+end
 
 number_list = [1,3,45,67,34,67]
-
-#Method to return the index of the searched number or nil if the integer in not present
-def search_number(integers_array, number)
-	count = 0
-	integers_array.each do |index, integer| 
-    if number[index] < integers_array.length && number[index] > 0
-    	p number[index]
-    elsif (number != integer) && (count == (integers_array.length-1))
-			count = nil
- 			p count
- 		else count+=1
-    	
-    end
-  end 
-end
-  
-search_number(number_list, 45)
-search_number(number_list, 4)
+p search_number(number_list, 45)
+p search_number(number_list, 4)
+puts "-----------------------"
 
 
+# Release 1
 
-#Release 1
-
-#Method that takes a number, put it in Fibonacci terms and returns an array of the terms.
+# Method that takes a number, put it in Fibonacci terms and returns an array of the terms.
 
 def fibo(number)
 	fibstring = [0,1]
