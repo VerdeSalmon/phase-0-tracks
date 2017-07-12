c30fc2cee445e5ca9da7fbbb03e6789117b59355
verdesalmon <iferiav@gmail.com>
2017-07-12 07:55:52 -0500

    Add hash drills solutions

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -7,6 +7,7 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 # printing each item in the array separated by an asterisk
 # ----
 zombie_apocalypse_supplies.each {|item| print "#{item}*"}
+puts "-----------------------------"
 
 # 2. Create a method to see if a particular item (string) is in the
 # zombie_apocalypse_supplies using #each.
@@ -22,10 +23,10 @@ def has_item?(list, item)
   return false
 end
 
-puts "\n-----------------------------"
+
 puts "zombie_apocalypse_supplies has boots? #{has_item?(zombie_apocalypse_supplies, "boots")}"
 puts "zombie_apocalypse_supplies has binoculars? #{has_item?(zombie_apocalypse_supplies,"binoculars")}"
-
+puts "-----------------------------"
 
 # 3. You can't carry too many things, you've only got room in your pack for 5.
 # Remove items in your zombie_apocalypse_supplies in any way you'd like,
@@ -34,12 +35,13 @@ puts "zombie_apocalypse_supplies has binoculars? #{has_item?(zombie_apocalypse_s
 
 zombie_apocalypse_supplies.each do |supply|
   if supply > supply[4]
-    zombie_apocalypse_supplies.delete(supply)
+    zombie_apocalypse_supplies.delete(supply) # #delete is destructive in Array
+    #zombie_apocalypse_supplies.slice(zombie_apocalypse_supplies.index(supply)).#slice doesn't modify the original array
   end
 end
 
-puts "-----------------------------"
 p zombie_apocalypse_supplies
+puts "-----------------------------"
 
 # 4. You found another survivor! This means you can combine your supplies.
 # Create a new combined supplies list out of your zombie_apocalypse_supplies
@@ -50,10 +52,8 @@ other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove",
                             "solar battery", "flashlight"]
 # ----
 
-puts "-----------------------------"
-
 p two_survivors_supplies = zombie_apocalypse_supplies | other_survivor_supplies
-
+puts "-----------------------------"
 
 # Hash Drills
 
@@ -71,14 +71,36 @@ extinct_animals = {
 # with a dash in between the key and value, and an asterisk between each pair.
 # ----
 
+extinct_animals.each do |animal, extinction_year|
+  print "#{animal}-#{extinction_year} * "
+end
+
+puts "\n-----------------------------"
+
 # 2. Keep only animals in extinct_animals if they were extinct before
 # the year 2000, using #each.
 # ----
+
+extinct_animals.each do |animal, extinction_year|
+  if extinction_year > 2000
+    extinct_animals.delete(animal)# #delete is destructive in Hash
+  end
+end
+
+p extinct_animals
+puts "-----------------------------"
 
 # 3. Our calculations were completely off, turns out all of those animals went
 # extinct 3 years before the date provided. Update the values in extinct_animals
 # using #each, so they accurately reflect what year the animal went extinct.
 # ----
+
+extinct_animals.each do |animal, extinction_year|
+  extinct_animals[animal] = extinction_year - 3
+end
+
+p extinct_animals
+puts "-----------------------------"
 
 # 4. You've heard that the following animals might be extinct, but you're not sure.
 # Build a method  using #each that checks if an animal is in the hash and returns true/false.
@@ -90,8 +112,24 @@ extinct_animals = {
 # Driver code example: is_extinct?(extinct_animals, "Andean Cat")
 # ----
 
+def is_extinct?(extinct_animals, possible_extinct_animal)
+  extinct_animals.each_key do |animal|
+    if possible_extinct_animal == animal
+      return true
+    end
+  end
+  return false
+end
+
+p is_extinct?(extinct_animals,"Andean Cat")
+p is_extinct?(extinct_animals,"Dodo")
+p is_extinct?(extinct_animals,"Saiga Antelope")
+puts "-----------------------------"
+
 # 5. We just found out that the Passenger Pigeon is actually not extinct!
 # Remove them from extinct_animals and return the key value pair as a two item array.
 # Find a Ruby Hash built-in method that helps you accomplish this or build
 # your own method using #each
 # ----
+
+p extinct_animals.shift
