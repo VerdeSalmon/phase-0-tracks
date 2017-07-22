f9988708f33858712942c142c42670b50e257005
verdesalmon <iferiav@gmail.com>
2017-07-21 21:16:49 -0500

    Add pseudocode for longuestWordFinder function

---
+++ js/algos.js
@@ -0,0 +1,13 @@
+// Release 0: Find the Longest Phrase
+
+// Created a function that returns the longest word or phrase of a list
+  // Input: word or phrases list
+  // Steps:
+    // Iterate through the list
+      // Compare the elements length 
+  // Output: the longest word or phrase of the list
+
+  
+function longestWord(wordList){
+
+}
