e830271bede853924d95a0052b02dcf2c6846f83
verdesalmon <iferiav@gmail.com>
2017-07-21 19:13:21 -0500

    Change pseudocode

--- js/explore.js
+++ js/explore.js
@@ -1,26 +1,11 @@
 // Create an object that takes a word or phrase an reverse it
-
-function reverse(word){
-	// Create an array to return the reverse string
-	var reverseString = [];
-	// Iterate trhough the word
-	for (var i = 0; i < word.length; i++) {
-	// Return each letter of the word, put on top last letter returned
-	reverseString.unshift(word[i]);
-	} 
-	//Return the reverse string
-	return reverseString.join("");
-}
+// Input: a word
+// Steps:
+  // Iterate trhough the word
+  // Countdown by index
+// Output: reversed string 
 
 
-//Call the object, give a string as an argument and store the result of the function in a variable
-var reverseWord = reverse("yellow chicken baby");
-
-
-//Print the variable when a condition become true
-if (reverseWord.length > 5){
-	console.log(reverseWord);
-}
 
 
 
