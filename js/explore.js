e1cbad332f221cabd2d2aa0a5a518185a294dcc4
verdesalmon <iferiav@gmail.com>
2017-07-21 19:17:15 -0500

    Simplify reverse function

--- js/explore.js
+++ js/explore.js
@@ -3,10 +3,17 @@
 // Steps:
   // Iterate trhough the word
   // Countdown by index
-// Output: reversed string 
+// Output: reversed word 
 
 
+function reverse(word){
+  var reverseWord = []
+  for (var i = word.length-1; i >=0; i--){
+    reverseWord +=word[i];
+  }
+  return reverseWord;
+}
 
 
-
+console.log(reverse("hola"));
 
