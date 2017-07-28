617253c4f07ae1d7cc01bfc6a0eae0530f79001f
verdesalmon <iferiav@gmail.com>
2017-07-28 16:55:15 -0500

    Add create_recipe and boolean_maker methods

--- databases/sql_ruby/has_nuts.rb
+++ databases/sql_ruby/has_nuts.rb
@@ -4,11 +4,39 @@ recipes_db = SQLite3::Database.new("recipes.db")
 recipes_db.results_as_hash = true
 
 create_table_cmd = <<-SQL
-  CREATE TABLE IF NOT EXISTS food(
+  CREATE TABLE IF NOT EXISTS recipe_list(
     id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
+    recipe_name VARCHAR(255),
     has_nuts BOOLEAN
   )
 SQL
 
-food_db.execute(create_table_cmd)
+recipes_db.execute(create_table_cmd)
+
+def create_recipe(recipes_db, recipe_name, has_nuts)
+  recipes_db.execute("INSERT INTO recipe_list (recipe_name, has_nuts) VALUES (?, ?)", [recipe_name, has_nuts])
+end  
+
+def boolean_maker(input)
+  if input == "y"
+     "true"
+  elsif input == "n"
+    "false"
+  else
+    nil
+  end
+end
+
+
+
+#------------- DRIVER CODE ---------------
+
+
+
+puts "what recipe you will like to suscribe" 
+recipe_name = gets.chomp.capitalize
+
+puts "That recipe has nuts? (Type 'y' or 'n'"
+has_nuts = boolean_maker(gets.chomp.downcase)
+
+create_recipe(recipes_db, recipe_name, has_nuts)
